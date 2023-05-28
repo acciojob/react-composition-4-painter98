18d91c70de9724f4001666a46838e514c14c9b19
painter98 <emailmepallavi@gmail.com>
2023-05-28 13:40:51 +0530

    change2

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React,{useState} from "react";
 import './../styles/App.css';
-import Input from './form.js';
+import Input from './Form.js';
 
 const App = () => {
   let [name,setName] = useState('default');
